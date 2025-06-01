543b5225151a9235a86b6cb4237a0992fd76200c
Fayzul Islam Faisal <mfbfaisal@gmail.com>
2025-06-01 18:44:12 +0600

    Add initial project setup with components, styles, and layout
    - Create components.json for configuration
    - Update package.json and package-lock.json with new dependencies
    - Add global styles in globals.css
    - Implement Dashboard layout and page components
    - Create reusable Button component
    - Add utility function for class name merging
    - Include logo and cover images
    - Remove default Next.js page

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className=" container mx-auto px-4">
+      <Button variant="link">Button</Button>
+    </div>
+  );
+}
