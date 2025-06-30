74c4e528062a152ac5cceda11cd438f12ebcf0c6
Fayzul Islam Faisal <mfbfaisal@gmail.com>
2025-06-30 13:56:37 +0600

    Refactor UpcomingProjects component for improved layout and styling; add Projector icon and enhance project description visibility

--- src/components/home-page/UpcomingProjects.tsx
+++ src/components/home-page/UpcomingProjects.tsx
@@ -2,7 +2,7 @@
 
 'use client';
 
-import { ArrowRight, BellRing, MoveRightIcon } from 'lucide-react';
+import { ArrowRight, BellRing, MoveRightIcon, Projector } from 'lucide-react';
 import Image, { StaticImageData } from 'next/image';
 import React from 'react';
 import { Button } from '../ui/button';
@@ -35,25 +35,25 @@ interface UpcomingProjectsProps {
 const UpcomingProjects: React.FC<UpcomingProjectsProps> = ({ sectionTitle, projects }) => {
 
   return (
-    <section className=" bg-[var(--custom-bg-white)] ">
+    <section className="bg-[var(--custom-bg-white)]">
       <div className="container mx-auto px-4  py-12 border-b-1 border-[var(--custom-bg-primary)]/10">
-        <div className="flex flex-col xl:flex-row items-center gap-4 lg:gap-8">
+        <div className="flex flex-col gap-4 md:gap-8">
           {/* Left Column */}
-          <div className="w-full lg:w-[100%] xl:w-[20%] flex flex-col items-center xl:items-start gap-4 group">
-            <div className="flex items-center gap-2 transition-all duration-300 ease-in-out">
-              <ArrowRight className="w-6 h-6 text-black opacity-75 transition-all duration-300 ease-in-out group-hover:opacity-100 group-hover:translate-x-1" />
-              <h2 className="text-2xl xl:text-xl text-black opacity-75 transition-all duration-300 ease-in-out group-hover:opacity-100 group-hover:tracking-wide">
+          <div className="flex flex-col items-center gap-3 group">
+            <div className="relative flex items-center justify-center w-full group">
+              <span className="absolute top-1/2 left-1/2 -translate-x-1/2 w-1/2 h-[2px] bg-[var(--custom-bg-primary)]/20 group-hover:bg-[var(--custom-bg-accent)]/75 z-0" />
+              <h2 className="relative z-10 bg-[var(--custom-bg-white)] px-4 text-2xl xl:text-3xl font-bold text-[var(--custom-text-primary)] text-center opacity-75 transition-all duration-300 ease-in-out group-hover:opacity-100 group-hover:tracking-wide">
                 {sectionTitle.title}
               </h2>
             </div>
-            <p className="text-black opacity-75 transition-all duration-300 ease-in-out group-hover:opacity-100 group-hover:translate-y-[1px]">
+            <p className="text-[var(--custom-text-primary)] opacity-75 transition-all duration-300 ease-in-out group-hover:opacity-100 group-hover:translate-y-[1px]">
               {sectionTitle.subTitle}
             </p>
           </div>
 
           {/* Right Column */}
-          <div className="w-full lg:w-[100%] xl:w-[80%]">
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-5 md:gap-4">
+          <div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 md:gap-4">
               {projects.map((project) => (
                 
                 <Card
@@ -83,7 +83,7 @@ const UpcomingProjects: React.FC<UpcomingProjectsProps> = ({ sectionTitle, proje
                               preserveAspectRatio="none" 
                               className="w-full h-full text-[var(--custom-text-primary)]/50 group-hover:text-[var(--custom-bg-primary)]/25 transition-all duration-300 ease-in-out"
                             >
-                              <path d="M0,12 C120,0 380,64 500,32 L500,100 L0,100 Z"
+                              <path d="M0,16 C120,0 380,64 500,32 L500,100 L0,100 Z"
                                   fill="currentColor"
                                   fillOpacity="0.8"
                                   className="transition-all duration-500 group-hover:fill-opacity-90 ease-in-out"
@@ -100,12 +100,12 @@ const UpcomingProjects: React.FC<UpcomingProjectsProps> = ({ sectionTitle, proje
                               {project.name}
                             </CardTitle>
                             <CardDescription
-                              className="
-                                mt-2 text-sm text-[var(--custom-text-secondary)]
-                                min-h-0 opacity-0 overflow-hidden
-                                group-hover:min-h-0 group-hover:opacity-100 group-hover:pb-2
-                                transition-all duration-300 ease-in-out
-                              "
+                                className="
+                                  mt-2 text-sm text-[var(--custom-text-secondary)]
+                                  max-h-0 visibility-hidden overflow-hidden
+                                  group-hover:max-h-40 group-hover:visibility-visible group-hover:pb-2
+                                  transition-all duration-300 ease-in-out
+                                "
                             >
                               {project.description}
                             </CardDescription>
@@ -115,7 +115,7 @@ const UpcomingProjects: React.FC<UpcomingProjectsProps> = ({ sectionTitle, proje
                                 variant="unstyled"
                                 className="btn-glow-accent flex items-center gap-2 text-[var(--custom-text-secondary)]"
                               >
-                                <MoveRightIcon className="w-4 h-4 animate-pulse" />
+                                <MoveRightIcon className="w-4 h-4 animate-pulse"/>
                                 View Details
                               </Button>
                               
