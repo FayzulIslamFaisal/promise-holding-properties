1b9dc12f96ed8ad60339d87e1e1b81509969e71f
Fayzul Islam Faisal <mfbfaisal@gmail.com>
2025-07-29 19:14:09 +0600

    ProjectBanner dark mode added

--- src/components/home-page/InsightsSection.tsx
+++ src/components/home-page/InsightsSection.tsx
@@ -124,6 +124,7 @@ const InsightsSection: React.FC = () => {
           <div className="">
             <Link
               href="/media-center"
+              target="_blank"
               className="btn-glow-accent flex items-center gap-2"
             >
               <span>View All</span>
--- src/components/project-details/ProjectBanner.tsx
+++ src/components/project-details/ProjectBanner.tsx
@@ -23,7 +23,7 @@ const ProjectBanner = ({ slug }: ProjectDetailWrapperProps) => {
           className="absolute inset-0 w-full h-full object-cover"
           aria-label={`Project banner video for ${slug}`}
         />
-        <div className="absolute inset-0 bg-[var(--custom-bg-primary)]/30" />
+        <div className="absolute inset-0 bg-[var(--custom-bg-primary)]/20 dark:bg-[var(--custom-bg-accent)]/20" />
         <div className="absolute inset-0 flex flex-col items-center justify-center text-center z-10 p-4">
           <motion.h1
             className="text-4xl md:text-6xl font-bold text-[var(--custom-text-white)] mb-6"
