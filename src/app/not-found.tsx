69f7d208ceaa9e5737c642f5e7281d057f8b5c3f
Fayzul Islam Faisal <mfbfaisal@gmail.com>
2025-08-14 16:21:47 +0600

    not found page re design

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,28 +1,38 @@
 import { RotateCcw } from 'lucide-react';
-import Link from 'next/link'
+import Image from 'next/image';
+import Link from 'next/link';
 
 const NotFound = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
-        <div className="flex flex-col items-center">
-            <h2 className="text-3xl mb-3">Oops! Page Not Found</h2>
-            <p className="mb-3 pb-4 text-xl text-[var(--custom-text-primary)]">
-                The page you{"'"} re looking for doesn’t exist or has been moved.
-            </p>
-        <Link href="/"
-            className="bg-[var(--custom-bg-accent)] text-[var(--custom-text-secondary)] py-2 px-6 rounded-md hover:bg-opacity-90 transition flex items-center gap-2 w-[240px] justify-center"
-        >
-            <RotateCcw className="size-5 md:size-6" />
-             Go Back to Homepage
-        </Link>
-</div>
+    <div className="relative w-full h-screen">
+      {/* Fullscreen background image */}
+      <Image
+        src="/assets/images/not-found.png"
+        alt="Page Not Found Background"
+        fill
+        className="object-cover"
+        priority
+      />
+      <div className="absolute inset-0 z-10 dark:bg-[var(--custom-bg-primary)]/40 bg-[var(--custom-bg-accent)]/20 backdrop-blur-sm" />
 
-      
+      {/* Overlay content */}
+      <div className="absolute inset-0 z-20 flex flex-col items-center justify-center text-center px-4">
+        <h2 className="text-4xl md:text-5xl font-bold mb-4 text-[var(--custom-text-white)] z-20">
+          Oops! Page Not Found
+        </h2>
+        <p className="text-lg md:text-xl mb-6 text-[var(--custom-text-white)] z-20">
+          The page you{"'"}re looking for doesn’t exist or has been moved.
+        </p>
+        <Link
+          href="/"
+          className="bg-[var(--custom-bg-accent)] text-[var(--custom-text-white)] py-4 px-6 rounded-md transition flex items-center gap-2 z-20"
+        >
+          <RotateCcw className="w-5 h-5" />
+          Back to Home
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default NotFound;
-
- 
-
