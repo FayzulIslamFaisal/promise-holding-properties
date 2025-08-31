3803b96c1c3ae54d76cadff843b0304be003bd9e
Fayzul Islam Faisal <mfbfaisal@gmail.com>
2025-08-31 13:21:07 +0600

    Refactor MediaCenterContentCard styles for consistency; update link to point to Insight page

--- src/components/media-center/MediaCenterContentCard.tsx
+++ src/components/media-center/MediaCenterContentCard.tsx
@@ -41,16 +41,16 @@ const MediaCenterContentCard: React.FC<MediaCenterContentCardProps> = ({ newslet
               <div className="absolute inset-0 bg-gradient-to-t from-[var(--custom-bg-primary)]/60 dark:from-[var(--custom-bg-accent)]/60 via-[var(--custom-bg-primary)]/40  dark:via-[var(--custom-bg-primary)]/40 to-[var(--custom-bg-white)]/10 dark:to-[var(--custom-bg-white)]/10 transition-opacity duration-500 group-hover:opacity-90" ></div>
             </div>
             <div className="pt-4 pb-6">
-              <h3 className="text-[var(--custom-text-primary)] dark:text-[var(--custom-text-white)] text-lg font-semibold mb-3 leading-tight">
+              <h3 className="darkLight-text-color text-lg font-semibold mb-3 leading-tight">
                 {newsletter.title}
               </h3>
-              <p className="text-[var(--custom-text-primary)] dark:text-[var(--custom-text-white)] text-sm font-medium pb-2">
+              <p className="darkLight-text-color text-sm font-medium pb-2">
                 {newsletter.date}
               </p>
             </div>
             <div className="flex items-center justify-between absolute bottom-0 left-6 ">
                 <Link
-                  href={`/media-center/${newsletter.slug}`}
+                  href={`/insight/${newsletter.slug}`}
                   className="btn-glow-accent"
                 >
                   View Newsletter
